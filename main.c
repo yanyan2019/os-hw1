d068852132c0bf4fcfd227cf13978b4f91496d3d
YanYan Lee <leeya@oregonstate.edu>
2020-10-12 21:55:36 -0700

    finish interactive functions

--- main.c
+++ main.c
@@ -15,39 +15,41 @@ struct movie *createMovie(char *currLine){
 	char *token = strtok_r(currLine, ",", &ptr);
 	currMovie->title = calloc(strlen(token)+1, sizeof(char));
 	strcpy(currMovie->title, token);
-//	printf("name: %s ", currMovie->title);	
 
 	// token for year
 	token = strtok_r(NULL, ",", &ptr);
 	currMovie->year = atoi(token);
-//	printf("year: %i ", currMovie->year);
 
 	// token for languages
 	token = strtok_r(NULL, ",", &ptr);
+
 	// remove the brackets
 	char *token2 = strtok_r(NULL, "[", &token);
 	token = strtok_r(NULL, "]", &token2);
+
 	// allocate dynamic array for languages 
-	currMovie->lang = calloc(strlen(token)+1, sizeof(char)*langSize);
-	int i;
-//	printf("lang: ");
+	currMovie->lang = (char**)calloc(langSize ,sizeof(char*));
+	
 	// dynamic array for languages
+	int i;
 	for(i = 0; i < langSize; i++){
-		currMovie->lang[i] = (char*)malloc(langSize+1);
-		currMovie->lang[i] = strtok_r(NULL, ";", &token);
-//		if(currMovie->lang[i] != NULL){
-//			
-//			printf("%s, ", currMovie->lang[i]);
-//		}
+		currMovie->lang[i] = (char*)calloc(strlen(token)+1 ,sizeof(char));
+		token2 = strtok_r(NULL, ";", &token);
+	
+		// fill the empty language with string
+		if(token2 == NULL){
+			strcpy(currMovie->lang[i], "NULL");
+
+		}else{
+			strcpy(currMovie->lang[i], token2);
+		}
+		
 	}	
-//	printf("\n");
 
 	// token for rating
 	token = strtok_r(NULL, ",", &ptr);
 	currMovie->rating = strtod(token, NULL);
-//	printf("rating: %.1f\n", currMovie->rating);
 
-	// token for 
 	// set the next node to null
 	currMovie->next = NULL;
 	return currMovie;	
@@ -93,12 +95,180 @@ struct movie *processFile(char *filePath, int *m_count){
 	return head;
 }
 
+// print all movies in a linked list
+void printMovie(struct movie * m){
+	while(m != NULL){
+		printf("%i %.1f %s\n",m->year, m->rating, m->title);
+		m = m->next;
+	}
+	printf("\n");
+}
+
+// show movies released in the specified year 
+void movieInSpecYr(struct movie *list){
+	printf("Enter the year of which you want to see movies: ");
+	bool exist = false;
+	int yr = 0;
+
+	// user input
+	scanf("%i", &yr);
+
+	// loop through the linked list
+	struct movie *m = list;
+	while(m != NULL){
+
+		// if year matches, print the title
+		if(m->year == yr){
+			printf("%s\n",m->title);
+			exist = true;
+		}
+		// go to the next node
+		m = m->next;
+	}
+	
+	// print if no matches movies
+	if(exist == false){
+		printf("No data about movies released in the year %i\n", yr);
+	}
+	printf("\n");	
+}
+
+// allocate new movie struct
+struct movie * createNewList(){
+	int i;
+	struct movie *m =(struct movie*)malloc(sizeof(struct movie));
+	m->title = calloc(100, sizeof(char));
+	m->lang = (char**)calloc(5 ,sizeof(char*));
+	for(i = 0;i < 5; i++){
+		m->lang[i] = (char*)calloc(50, sizeof(char));
+	}
+	return m;
+}
+
+//copy data form movie2 to movie
+void copyMovie(struct movie *m,  struct movie *m2){
+	int i;
+	strcpy(m->title, m2->title);
+	m->year = m2->year;
+  	for(i = 0; i < 5; i++){
+		strcpy(m->lang[i], m2->lang[i]);
+	}
+	m->rating = m2->rating;
+	m->next = m2->next;
+}
+
+// show highest rated movie in the specific year
+void highRatedMovInSpecYr(struct movie *list, int m_count){
+		
+	bool exist = false;
+
+	// origin list
+	struct movie *temp = list;
+
+	// temporary holder for replacing	
+	struct movie *temp2 = createNewList();
+
+	// New list after sorting
+	struct movie *m = createNewList();
+	struct movie *m2 = m;
+
+	//head of the new list
+	copyMovie(m ,temp);
+	m->next = NULL;
+
+	// loop through the original list for comparsion
+	while(temp != NULL){
+
+		// loop through the new list
+		while(m2 != NULL){
+			
+			// check for replacing condition
+			if(m2->year == temp->year){
+
+				// if original list has larger rating movie, then copy the data from it to the new list
+				if(m2->rating < temp->rating){
+					copyMovie(temp2, m2);
+					copyMovie(m2, temp);
+					m2->next = temp2->next;
+					exist = true;
+					temp = temp->next;
+
+				}else{
+					// if nothing is changing
+					break;
+				}
+			}else if(m2->year != temp->year){ 
+				
+				// insert the new node at the end of the new list
+				if(m2->next == NULL){
+
+					//insert new node
+					struct movie * newNode = createNewList();
+					copyMovie(newNode, temp);
+					newNode->next = NULL;
+					m2->next = newNode;
+	
+					// back to the head of the new list
+					m2 = m;
+					temp = temp->next;
+				}else{
+					// continute to loop if not at the end of the new list
+					m2 = m2->next;
+				}
+			}
+		}
+		temp = temp->next;
+		m2 = m;
+	}
+	printMovie(m);
+	free(m);
+	free(temp2);
+}
+
+// show the title and year of release of all movies in a specific languages
+void movInSpecLang(struct movie * list){
+	int i;
+
+	// check if any matches results
+	bool exist = false;
+
+	char user[50];
+	struct movie *m = list;
+	printf("Enter the language for which you want to see movies: ");
+	scanf("%s", &user);
+
+	// loop through the list to matches languages
+	while(m != NULL){
+
+		// for each language in the movie
+		for(i = 0; i < 5; i++){
+
+			// if matches, print the movie
+			int j = strcmp(user, m->lang[i]);
+			if(j == 0){
+				printf("%i %s\n", m->year, m->title);
+			
+				// no need for data no found message
+				exist = true;
+			}
+		}
+		m = m->next;	
+	}
+	printf("\n");
+	
+	// print if no data found
+	if(exist == false){
+		printf("No matches result for movies in %s \n\n", user);
+	}
+}
+
 // user interactive function
-void interactive(){
+void interactive(struct movie * list, int m_count){
 	int input = 0;
 	// loop until user exit
 	while(input != 4){
 
+		// print user options
 		printf("1. Show movies released the in the sepcified year\n");
 		printf("2. Show highest rated movie for each year\n");
 		printf("3. Show the title and year of release of all movies in a specific language\n");
@@ -106,11 +276,18 @@ void interactive(){
 		printf("Enter a choice from 1 to 4: ");
 
 		scanf("%i", &input);
-		if(input > 4 || input < 1){
+
+		// call differents functions for inputs
+		if(input == 1){
+			movieInSpecYr(list);
+		}else if(input == 2){
+			highRatedMovInSpecYr(list, m_count);
+		}else if(input == 3){
+			movInSpecLang(list);
+		}else if(input > 4 || input < 1){
 			printf("You entered an incorrect choice. Try again.\n\n");
 		}
 	}
-	return;
 }
 
 int main(int argc, char *argv[]){
@@ -127,7 +304,7 @@ int main(int argc, char *argv[]){
   	printf("Processed file %s and parsed data for %i movies\n\n", argv[1], m_count);
 
 	// call inteactive function
-	interactive();		
-
+	interactive(list,m_count);
+	
   	return EXIT_SUCCESS;
 }
